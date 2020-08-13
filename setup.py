9b47b7748558e5f5e86bf207104fa29298c4c022
Rashi0911 <59896588+Rashi0911@users.noreply.github.com>
2020-08-13 00:19:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-import setuptools
-
-setuptools.setup(
-    name="Test_package", 
-    version="1.0.0",
-    author="Rashi",
-    author_email="rashi9.agarwal96@gmail.com",
-    description="a test package",
-    url="https://github.com/adarshtri/510-hw1",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-    ],
-    python_requires='>=3.6',
+from setuptools import setup
+setup(
+    name='code_package',
+    version='v0.0.0',
+    packages=['code'],
+    url='https://github.com/adarshtri/510-hw1',
+    license='',
+    author='rashi',
+    author_email='rashi9.agarwal96@gmail.com',
+    description='HW1- Sample structure of repo'
 )
