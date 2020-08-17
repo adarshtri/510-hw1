c3ffe3009f306b0c151be246c22d8d8d016860c5
Ayushi61 <54721860+Ayushi61@users.noreply.github.com>
2020-08-16 21:16:11 -0400

    Update setup.py
    find_packages()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 setup(
     name='code_package',
     version='v0.0.0',
-    packages=packages=find_packages(),
+    packages=find_packages(),
     url='https://github.com/adarshtri/510-hw1',
     license='',
     author='rashi',
