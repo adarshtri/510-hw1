86264c8ab5863d306a5a2f462c944ac929f7a288
Ayushi61 <54721860+Ayushi61@users.noreply.github.com>
2020-08-16 21:15:25 -0400

    Update setup.py
    Rectified setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 setup(
     name='code_package',
     version='v0.0.0',
-    packages=['code'],
+    packages=packages=find_packages(),
     url='https://github.com/adarshtri/510-hw1',
     license='',
     author='rashi',
